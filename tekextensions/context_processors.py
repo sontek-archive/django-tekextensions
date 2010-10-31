8b31b6d3d26ef7d52689b8ccf0ce8d5dbbd9a357
John Anderson <sontek@gmail.com>
2010-10-31 02:05:45 -0400

    added a context processor to expose the current site

--- tekextensions/context_processors.py
+++ tekextensions/context_processors.py
@@ -1,4 +1,18 @@
 from django.conf import settings
+from django.contrib.sites.models import Site, RequestSite
 
 def admin_media_prefix(request):
     return {'ADMIN_MEDIA_PREFIX': settings.ADMIN_MEDIA_PREFIX }
+
+def current_site(request):
+    '''
+    A context processor to add the "current_site" to the current Context
+    '''
+    context_name = 'CURRENT_SITE'
+
+    try:
+        current_site = Site.objects.get_current()
+        return { name: current_site, }
+    except Site.DoesNotExist:
+        # always return a dict, no matter what!
+        return {name: RequestSite(request)}
