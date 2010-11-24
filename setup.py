d35620be3d223a2b05982b78f512e4bc617c2327
John Anderson <sontek@gmail.com>
2010-11-23 23:38:00 -0600

    started to refactor so its more re-usable

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='django-tekextensions',
     version=__import__('tekextensions').__version__,
     description='A set of re-usable widgets and commands for django',
-    long_description=open('README.markdown').read(),
+    long_description='',
     author='John Anderson',
     author_email='sontek@gmail.com',
     url='http://github.com/sontek/django-tekextensions',
--- tekextensions/views.py
+++ tekextensions/views.py
@@ -1,38 +1,32 @@
-from django.forms.models import modelform_factory
-from django.db.models.loading import get_models, get_app, get_apps
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import HttpResponse
 from django.utils.html import escape
+from tekextensions.forms import get_model_form, normalize_model_name
 
-@login_required
-def add_new_model(request, model_name):
-    if (model_name.lower() == model_name):
-        normal_model_name = model_name.capitalize()
-    else:
-        normal_model_name = model_name
-    app_list = get_apps()
-    for app in app_list:
-        for model in get_models(app):
-            if model.__name__ == normal_model_name: 
-                form = modelform_factory(model)
+def add_new_model(request, model_name, form=None):
+    normal_model_name = normalize_model_name(model_name)
 
-                if request.method == 'POST':
-                    form = form(request.POST)
-                    if form.is_valid():
-                        try:
-                            new_obj = form.save()
-                        except forms.ValidationError, error:
-                            new_obj = None
+    if not form:
+        form = get_model_form(normal_model_name)
 
-                        if new_obj:
-                            return HttpResponse('<script type="text/javascript">opener.dismissAddAnotherPopup(window, "%s", "%s");</script>' % \
-                                (escape(new_obj._get_pk_val()), escape(new_obj)))
+    if form:
+        if request.method == 'POST':
+            form = form(request.POST)
+            if form.is_valid():
+                try:
+                    new_obj = form.save()
+                except forms.ValidationError, error:
+                    new_obj = None
 
-                else:
-                    form = form()
+                if new_obj:
+                    return HttpResponse('<script type="text/javascript">opener.dismissAddAnotherPopup(window, "%s", "%s");</script>' % \
+                        (escape(new_obj._get_pk_val()), escape(new_obj)))
 
-                page_context = {'form': form, 'field': normal_model_name}
-                return render_to_response('popup.html', page_context, context_instance=RequestContext(request))
-    raise Exception('Did not find the model %s' % (normal_model_name))
+        else:
+            form = form()
+
+        page_context = {'form': form, 'field': normal_model_name}
+        return render_to_response('popup.html', page_context, context_instance=RequestContext(request))
+
